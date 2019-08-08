61c47b57df0f8dc9a3ed86c6ef75a458939b758b
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-08-08 18:09:44 -0400

    Update scripts.js
    Applied For Loop To Pokedex Array & Capitalized Type Properties

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,36 @@ var repository = [ /* Pokedex Object Array*/
   {
       name:'Blubasaur',
       height:2.04,
-      type:['grass','poison']
+      type:['Grass','Poison']
   },
   {
       name:'Charizard',
       height:5.07,
-      type:['fire','flying']
+      type:['Fire','Flying']
   },
   {
       name:'Pikachu',
       height:1.04,
-      type:['electric']
+      type:['Electric']
   },
   {
       name:'Dragonite',
       height:7.03,
-      type:['dragon','flying']
+      type:['Dragon','Flying']
   },
   {
       name:'Gyarados',
       height:21.04,
-      type:['water','flying']
+      type:['Water','Flying']
   }
 ];
 
-document.write(repository[0].name+" "+repository[0].height+" "+repository[0].type) /* Demonstration of Property Value Retrival From Pokedex Object Array */
+for(var i=0;i<repository.length;i++){ /* For Loop Applied To Pokedex Object Array */
+
+  document.write("<br>"+"Name: "+repository[i].name + "  | Height: "+repository[i].height+" | Type: "+repository[i].type);
+
+  if(repository[i].height > 5){
+       document.write("  (- Wow that is a big Pokemon!)")
+}
+  document.write("<p>");
+}
