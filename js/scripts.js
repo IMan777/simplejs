6d1349846a111295305e46b78b907d4e52ec3c7a
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-08-07 20:15:38 -0400

    Update scripts.js
    Removed Previous JS Code & Replaced With Pokedex Object Array & Property Retrival Demo

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
-alert('Hello World!');
+var repository = [ /* Pokedex Object Array*/
+  {
+      name:'Blubasaur',
+      height:2.04,
+      type:['grass','poison']
+  },
+  {
+      name:'Charizard',
+      height:5.07,
+      type:['fire','flying']
+  },
+  {
+      name:'Pikachu',
+      height:1.04,
+      type:['electric']
+  },
+  {
+      name:'Dragonite',
+      height:7.03,
+      type:['dragon','flying']
+  },
+  {
+      name:'Gyarados',
+      height:21.04,
+      type:['water','flying']
+  }
+];
 
-var favoriteFood = "My favorite food is avocado!";
-
-document.write(favoriteFood);
+document.write(repository[0].name+" "+repository[0].height+" "+repository[0].type) /* Demonstration of Property Value Retrival From Pokedex Object Array */
